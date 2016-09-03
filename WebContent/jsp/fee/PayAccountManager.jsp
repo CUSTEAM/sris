e749ac150791fcb4caf57edc502bda8041079e80
John <lol.hsiao@gmail.com>
2016-09-03 16:11:39 +0800

    新增轉帳資料管理

---
+++ WebContent/jsp/fee/PayAccountManager.jsp
@@ -0,0 +1,217 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>轉帳資料管理</title>
+<link href="/eis/inc/css/jquery-ui.css" rel="stylesheet"/>
+<link href="/eis/inc/bootstrap/plugin/bootstrap-fileinput/css/fileinput.min.css" rel="stylesheet">
+
+<script src="/eis/inc/js/plugin/bootstrap-typeahead.js"></script>
+<script src="/eis/inc/bootstrap/plugin/bootstrap-fileinput/js/fileinput.min.js"></script>
+<script src="/eis/inc/bootstrap/plugin/bootstrap-fileinput/js/fileinput_locale_zh-TW.js"></script>
+<link href="/eis/inc/bootstrap/plugin/silviomoreto-bootstrap-select/css/bootstrap-select.min.css" rel="stylesheet"/>
+<script src="/eis/inc/bootstrap/plugin/silviomoreto-bootstrap-select/js/bootstrap-select.min.js"></script>
+<script src="/eis/inc/bootstrap/plugin/silviomoreto-bootstrap-select/js/i18n/defaults-zh_TW.min.js"></script>
+<style>
+.file-caption-name{width:250px;}
+}
+</style>
+<script src="/eis/inc/js/plugin/stupidtable.min.js"></script>
+<script>$("#table").stupidtable();</script>
+</head>
+<body>    
+<div class="bs-callout bs-callout-info">
+<h4>轉帳資料管理</h4> <small>批次匯入請先下載最新格式，依欄位貼上資料，並按下匯入資料</small>
+</div>
+
+<form action="PayAccountManager" method="post" class="form-inline" enctype="multipart/form-data">
+
+
+
+<div class="panel panel-primary">
+<div class="panel-heading">新增或查詢</div>
+<table class="table">
+	<tr>
+		<td>
+		<%@ include file="/inc/jsp-kit/dhnSelector.jsp"%>	
+		<%int y=Integer.parseInt(request.getServletContext().getAttribute("school_year").toString()); %>
+		<select class="selectpicker" data-width="fit" name="year">			
+			<%for(int i=y; i>=y-4; i--){%>
+			<option <%if(String.valueOf(request.getAttribute("year")).equals(String.valueOf(i))){%>selected<%}%> value="<%=i%>"><%=i%>學年</option>
+			<%}%>			
+		</select>
+		<select class="selectpicker" data-width="fit" name="term">
+			<option <c:if test="${term eq''}">selected</c:if> value="">全部學期</option>
+			<option <c:if test="${term =='1'}">selected</c:if> value="1">第1學期</option>
+			<option <c:if test="${term =='2'}">selected</c:if> value="2">第2學期</option>
+		</select>
+		<select class="selectpicker" data-width="fit" name="Kind" id="Kind">			
+			<c:forEach items="${CODE_DIPOST}" var="c">
+			<option <c:if test="${Kind == c.id}">selected</c:if> value="${c.id}">${c.name}</option>
+			</c:forEach>
+		</select>
+		<button class="btn btn-danger" name="method:search">查詢</button>
+		</td>
+	</tr>
+	<tr>
+		<td nowrap>
+		<div class="input-group" style="width:300px;">
+			<div class="input-group-addon">學號</div>
+			<input class="form-control" onClick="this.value=''" autocomplete="off" type="text" id="StudentNo" value="${StudentNo}" name="StudentNo"
+		 data-provide="typeahead" placeholder="學號或姓名片段" />
+		</div>
+		<div class="input-group" style="width:120px;">
+			<div class="input-group-addon">局號</div>
+			<input type="text" name="OfficeNo" value="${OfficeNo}" id="OfficeNo" class="form-control" autocomplete="off" placeholder="局號"/>
+		</div>
+		<div class="input-group" style="width:200px;">
+			<div class="input-group-addon">帳號</div>
+			<input type="text" name="AcctNo" value="${AcctNo}" id="AcctNo" class="form-control" autocomplete="off" placeholder="帳號"/>
+		</div>
+		
+		</td>
+	</tr>
+	
+	<tr>
+		<td>
+		<select class="form-control" data-width="fit" name=occur_month id="occur_month">			
+			<option value="">選擇月份</option>
+			<c:forEach begin="1" end="12" varStatus="i">
+			<option <c:if test="${i.index == occur_month}">selected</c:if> value="${i.index}">${i.index}月份</option>
+			</c:forEach>
+		</select>
+		<div class="input-group">
+			<span class="input-group-addon" id="sizing-addon2">金額</span>
+			<input type="text" class="form-control" placeholder="$$$" name="Money" />
+			<span class="input-group-btn">
+			<button class="btn btn-danger" name="method:add">新增</button>	
+			
+			</span>
+		</div>
+		<div class="btn-group">		
+		<a href="PayAccountManager" class="btn btn-default">重設條件</a>
+		</div>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<table>
+			<tr>
+				<td>
+				<a class="btn btn-primary" href="jsp/fee/sample.xlsx"><span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span>下載格式</a>
+				</td>
+				<td style="padding:0px 5px 0px 5px;">
+				<input name="upload" type="file" 
+				data-show-preview="false" 
+				data-show-upload="false" 
+				id="upload" 
+				class="file"/>
+				<script>
+				$("#upload").fileinput({
+					multiple: false,			        
+				    language: "zh-TW",
+				    uploadUrl: "",
+				    allowedFileExtensions: ["xls", "xlsx"]
+				});
+				</script>
+				</td>			
+				<td>
+				<button class="btn btn-primary" id="saveTxtFile" name="method:upload" type="submit"><span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>匯入資料</button>
+				</td>
+			</tr>
+		</table>
+		
+		
+		
+		</td>
+	</tr>
+</table>
+</div>
+
+
+<c:if test="${!empty dipost}">
+<div class="panel panel-primary">
+<div class="panel-heading">新增或查詢</div>
+
+<table id="table" class="table table-hover" id="table">
+	<thead>
+	<tr class="text-info">
+		<th></th>
+		<th nowrap data-sort="string">種類</th>
+		<th nowrap data-sort="int">學年</th>
+		<th nowrap data-sort="int">學期</th>
+		<th nowrap data-sort="int">月份</th>
+		<th nowrap data-sort="string">班級</th>
+		<th nowrap data-sort="string">學號</th>
+		<th nowrap data-sort="string">姓名</th>		
+		<th nowrap data-sort="int">局號</th>
+		<th nowrap data-sort="int">帳號</th>
+		<th nowrap data-sort="int">金額</th>
+		<th nowrap data-sort="string">編輯日期</th>
+		
+	</tr>
+	</thead>
+	<c:forEach items="${dipost}" var="c">
+	<tr>
+		<td><input type="checkBox" name="del" value="${c.Oid}" /></td>
+		<td nowrap>${c.name}</td>
+		<td nowrap>${c.SchoolYear}</td>
+		<td nowrap>${c.SchoolTerm}</td>
+		<td nowrap>${c.occur_month}</td>
+		<td nowrap>${c.ClassName}</td>
+		<td nowrap>${c.student_no}</td>
+		<td nowrap>${c.student_name}</td>		
+		<td nowrap>${c.OfficeNo}</td>
+		<td nowrap>${c.AcctNo}</td>
+		<td nowrap>${c.Money}</td>
+		<td nowrap>${c.LastModified}</td>
+	</tr>
+	</c:forEach>
+</table>
+<div class="panel-body">
+<button class="btn btn-primary" name="method:del" type="submit"><span class="glyphicon glyphicon-cloud-upload" aria-hidden="true"></span>刪除勾選</button>
+
+</div>
+</c:if>
+
+
+
+
+
+
+
+</form>
+<script>  
+$(document).ready(function() {
+	//$("#occur_month").hide("slow");
+	
+	/*$("#Kind").change(function(){
+		if($(this).val()==3){
+			$("#occur_month").show("slow");
+		}else{
+			$("#occur_month").hide("slow");
+		}
+	});*/
+	
+	$("#StudentNo").typeahead({
+		//remote:"#student_no",
+		source : [],
+		items : 10,
+		updateSource:function(inputVal, callback){			
+			$.ajax({
+				type:"POST",
+				url:"/eis/autoCompleteStmd",
+				dataType:"json",
+				data:{length:10, nameno:inputVal},
+				success:function(d){
+					callback(d.list);
+				}
+			});
+		}		
+	});	
+});
+</script>
+</body>
+</html>
---
+++ src/action/fee/PayAccountManager.java
@@ -0,0 +1,316 @@
+package action.fee;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Message;
+import model.Stmd;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import service.impl.DataFinder;
+import action.BaseAction;
+
+public class PayAccountManager extends BaseAction{
+	
+	public File upload;	
+	public String occur_month, cno, tno, year, term, StudentNo, OfficeNo, AcctNo, Money, Kind, del[];
+	SimpleDateFormat sf=new SimpleDateFormat("yyyyMMdd");	
+	Date birthday;
+	Calendar c=Calendar.getInstance();
+	public String execute(){
+		
+		return SUCCESS;
+	}
+	
+	public String search(){
+		StringBuilder sql=new StringBuilder("SELECT d.Oid,s.student_no, s.student_name, "
+		+ "c.ClassName, cd.name, d.OfficeNo, d.AcctNo,d.Money, d.LastModified, d.SchoolYear, d.SchoolTerm, d.occur_month FROM "
+		+ "stmd s, Dipost d, Class c, CODE_DIPOST cd WHERE cd.id=d.Kind AND "
+		+ "s.student_no=d.StudentNo AND s.depart_class=c.ClassNo AND "
+		+ "d.SchoolYear='"+year+"' AND c.CampusNo='"+cno+"'"
+		+ "AND c.SchoolType='"+tno+"'AND d.Kind='"+Kind+"'");		
+		if(StudentNo.indexOf(",")>0)sql.append("AND StudentNo LIKE'"+StudentNo.substring(0, StudentNo.indexOf(","))+"'");
+		if(!occur_month.equals(""))sql.append("AND occur_month='"+occur_month+"'");
+		if(!term.equals(""))sql.append("AND d.SchoolTerm='"+term+"'");
+		request.setAttribute("dipost", df.sqlGet(sql.toString()));
+		return SUCCESS;
+	}
+	
+	public String del(){
+		Message msg=new Message();
+		if(del!=null){
+			for(int i=0; i<del.length; i++){
+				df.exSql("DELETE FROM Dipost WHERE Oid="+del[i]);				
+			}
+			msg.setSuccess("已刪除"+del.length+"筆資料");
+		}else{
+			msg.setSuccess("未勾選資料");
+		}
+		this.savMessage(msg);
+		return search();
+	}
+	
+	public String add(){
+		Message msg=new Message();
+		if(Money.equals("")|| AcctNo.equals("")|| OfficeNo.equals("")|| StudentNo.indexOf(",")<1){
+			msg.setError("欄位不完整");
+			this.savMessage(msg);
+			return SUCCESS;
+		}
+		try{
+			if(Kind.equals("3")){
+				df.exSql("INSERT INTO Dipost(StudentNo,OfficeNo,AcctNo,Money,Kind,Modifier,SchoolYear,SchoolTerm,occur_month)"
+				+ "VALUES('"+StudentNo.substring(0, StudentNo.indexOf(","))+"', '"+OfficeNo+"', '"+AcctNo+"', '"
+				+Money+"', '"+Kind+"', '"+getSession().getAttribute("userid")+"', '"+year+"', '"+term+"','"+occur_month+"');");
+			}else{
+				df.exSql("INSERT INTO Dipost(StudentNo,OfficeNo,AcctNo,Money,Kind,Modifier,SchoolYear,SchoolTerm)"
+				+ "VALUES('"+StudentNo.substring(0, StudentNo.indexOf(","))+"', '"+OfficeNo+"', '"+AcctNo+"', '"
+				+Money+"', '"+Kind+"', '"+getSession().getAttribute("userid")+"', '"+year+"', '"+term+"');");
+			}			
+			msg.setSuccess("新增完成");
+			this.savMessage(msg);
+			return search();
+		}catch(Exception e){
+			msg.setError("請檢查是否為重複資料");
+			this.savMessage(msg);
+			return search();
+		}
+	}
+	
+	
+	
+	public String upload() throws IOException{
+		Message msg=new Message();
+		if(upload==null){			
+			msg.setError("未指定檔案");
+			this.savMessage(msg);
+			return SUCCESS;
+		}
+		
+		FileInputStream fis = new FileInputStream(upload);		
+		XSSFWorkbook xwb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = xwb.getSheetAt(0);
+		XSSFRow row;	
+		
+		int check=0;
+		boolean b=false;
+		String y;
+		String t;
+		String k;
+		String s;
+		String o;
+		String a;
+		String m;
+		String mm;
+		for (int i = sheet.getFirstRowNum(); i <= sheet.getPhysicalNumberOfRows(); i++) {				
+			row = sheet.getRow(i);
+			if(row!=null){	
+				if(i==0)
+				try{//檢查標題
+					Integer.parseInt(row.getCell(0).toString());
+				}catch(Exception e){
+					msg.setError("發現標題欄位, 已跳過讀取<br>");
+					continue;
+				}
+				
+				for(int j=0; j<=6; j++){//檢查欄位null					
+					if(String.valueOf(row.getCell(j)).trim().equals("")||row.getCell(j)==null){	
+						msg.addError("第"+i+"行發現空白");
+						b=true;
+					}
+				}
+				
+				if(b==true){
+					msg.addError("<br>發現嚴重問題, 文件未匯入");
+					this.savMessage(msg);
+					return SUCCESS;
+				}
+				y=readCellAsString(row.getCell(0));
+				t=readCellAsString(row.getCell(1));
+				k=readCellAsString(row.getCell(2));
+				s=readCellAsString(row.getCell(3));
+				o=readCellAsString(row.getCell(4));
+				a=readCellAsString(row.getCell(5));
+				m=readCellAsString(row.getCell(6));	
+				mm=readCellAsString(row.getCell(7));
+				try{
+					//試刪除原資料
+					df.exSql("DELETE FROM Dipost WHERE Kind='"+k+"'AND StudentNo='"+s+"' AND SchoolYear='"+y+"' AND SchoolTerm='"+t+"'");
+					df.exSql("INSERT INTO Dipost(StudentNo,OfficeNo,AcctNo,Money,Kind,Modifier,SchoolYear,SchoolTerm,occur_month)"
+					+ "VALUES('"+s+"', '"+o+"', '"+a+"', '"+m+"', '"+k+"', '"+getSession().getAttribute("userid")+"', '"+y+"', '"+t+"','"+mm+"');");
+					check+=1;
+					
+				}catch(Exception e){
+					msg.addError("第"+i+"行在儲存中發生錯誤");
+					continue;
+				}										
+			}			
+		}
+		
+		fis.close();
+		//request.setAttribute("complete", complete);
+		//request.setAttribute("fail", fail);
+		msg.setSuccess("文件匯入完成, 共"+check+"筆資料");
+		this.savMessage(msg);
+		return SUCCESS;
+	}
+	
+	
+	private void saveTxtFile() throws IOException{
+		
+		FileInputStream fis = new FileInputStream(upload);
+		FileReader fr = new FileReader(fis.getFD());
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine(); //讀第一行
+		df.exSql("INSERT INTO LC_exam_stmds (student_no) VALUES ('"+line+"')ON DUPLICATE KEY UPDATE student_no=VALUES(student_no);");
+		StringBuffer sb = new StringBuffer();
+		while ((line=br.readLine())!=null){
+			df.exSql("INSERT INTO LC_exam_stmds (student_no) VALUES ('"+line+"')ON DUPLICATE KEY UPDATE student_no=VALUES(student_no);");
+		}	
+		fis.close();
+		fr.close();
+		br.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	/**
+	 * excel欄位強制轉文字 2003-
+	 * @param cell
+	 * @return
+	 */
+	private String readCellAsString(HSSFCell cell) {
+		if (cell == null) {
+			return null;
+		}
+		final DecimalFormat df = new DecimalFormat("####################0.##########");
+
+		switch (cell.getCellType()) {
+		case HSSFCell.CELL_TYPE_BLANK:
+			return "";
+		case HSSFCell.CELL_TYPE_BOOLEAN:
+			return Boolean.valueOf(cell.getBooleanCellValue()).toString();
+		case HSSFCell.CELL_TYPE_NUMERIC:
+			return df.format(cell.getNumericCellValue());
+		case HSSFCell.CELL_TYPE_STRING:
+			return cell.getStringCellValue();
+		case HSSFCell.CELL_TYPE_FORMULA:
+			return cell.getCellFormula();
+		case HSSFCell.CELL_TYPE_ERROR:
+			return Byte.toString(cell.getErrorCellValue());
+		default:
+			return "##POI## Unknown cell type";
+		}
+	}
+	
+	/**
+	 * excel欄位強制轉文字 2003+
+	 * @param cell
+	 * @return
+	 */
+	private String readCellAsString(XSSFCell cell) {
+		if (cell == null) {
+			return null;
+		}
+		final DecimalFormat df = new DecimalFormat("####################0.##########");
+
+		switch (cell.getCellType()) {
+		case HSSFCell.CELL_TYPE_BLANK:
+			return "";
+		case HSSFCell.CELL_TYPE_BOOLEAN:
+			return Boolean.valueOf(cell.getBooleanCellValue()).toString().trim();
+		case HSSFCell.CELL_TYPE_NUMERIC:
+			return df.format(cell.getNumericCellValue()).trim();
+		case HSSFCell.CELL_TYPE_STRING:
+			return cell.getStringCellValue().trim();
+		case HSSFCell.CELL_TYPE_FORMULA:
+			return cell.getCellFormula().trim();
+		case HSSFCell.CELL_TYPE_ERROR:
+			return Byte.toString(cell.getErrorCellValue());
+		default:
+			return "##POI## Unknown cell type";
+		}
+	}
+	
+	/**
+	 * 班級是否存在
+	 * @param ClassNo
+	 * @return
+	 */
+	private boolean checkClass(String ClassNo){
+		if(df.sqlGetInt("SELECT COUNT(*)FROM Class WHERE ClassNo='"+ClassNo+"'")<1){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * 取生日
+	 * @param str
+	 * @return
+	 * @throws ParseException
+	 */
+	private Date getBirthday(String str) throws ParseException{		
+		if(str.length()==6){
+			str="00"+str;
+		}else{
+			str="0"+str;
+		}		
+		
+		birthday=sf.parse(str);
+		c.setTime(birthday);
+		c.add(Calendar.YEAR, 1911);
+		return c.getTime();
+	}
+	
+	/**
+	 * 取性別
+	 * @param idno
+	 * @return
+	 */
+	private String getSex(String idno){	
+		try{
+			return idno.substring(1,2);
+		}catch(Exception e){
+			return "1";
+		}
+	}
+	
+	/**
+	 * 訊息
+	 * @param stdNo
+	 * @param msg
+	 * @return
+	 */
+	private Map setMsg(String stdNo, String msg){
+		Map m=new HashMap();
+		m.put("stdNo", stdNo);
+		m.put("msg", msg);
+		return m;
+	}
+
+}
